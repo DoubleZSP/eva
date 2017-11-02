faa826f95107c27038949b6ec65945587dd7deec
DpubleZSP <519857696@qq.com>
2017-11-02 22:52:39 +0800

    aaaaa

--- src/main/java/com/card/spring/business/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.card.spring.business;
-
-import com.card.spring.entity.User;
-import java.util.List;
-
-public interface IUserService {
-	public boolean checkUser(User user);
-	public List<User> loadAll();
-}
